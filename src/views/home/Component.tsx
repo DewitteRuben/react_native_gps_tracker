a9410c7120d92fe249fb3090eb93405fe8943cdf
Ruben Dewitte <ruben.dewitte@proximus.be>
2020-02-04 20:35:06 +0100

    refactor: revamped home page

--- src/views/home/Component.tsx
+++ src/views/home/Component.tsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Image } from "react-native";
 import { NavigationStackScreenComponent } from "react-navigation-stack";
 import { useNavigation } from "react-navigation-hooks";
 import Button from "../../components/Button";
 import Overlay from "../../components/Overlay";
-import { CText } from "../../components";
+import { CText as Text, Modal } from "../../components";
 import { GLOBAL } from "../../styles/global";
 import styles from "./styles";
 import { ROUTES } from "../../navigators/navigation";
+import { getModalButtons } from "../../utils/modal";
 
 const headerImage = require("../../assets/images/header-travel.jpg");
 
 const Home: NavigationStackScreenComponent = () => {
   const { navigate } = useNavigation();
+  const [showModal, setModalVisibility] = useState(false);
+
+  const modalButtons = getModalButtons({ label: "Ok", callback: () => setModalVisibility(false) });
 
   const goToApp = () => navigate(ROUTES.TAB_MAP);
 
@@ -22,20 +26,24 @@ const Home: NavigationStackScreenComponent = () => {
         <Image resizeMode="cover" style={styles.splashImage} source={headerImage} />
       </Overlay>
       <View style={styles.titleContainer}>
-        <CText variant="h1" bold text="Welcome!" />
-        <CText text="Please select your preffered routing method:" />
+        <Text variant="h1" bold text="Welcome!" />
+        <Text text="Read more about tracker or click 'Start tracking' to begin!" />
       </View>
       <View style={[GLOBAL.LAYOUT.flexCenter, styles.mainContainer]}>
-        <Button text="Routed Tracking" block onPress={goToApp} />
+        <Button text="Start tracking" block onPress={goToApp} />
         <View style={styles.orContainer}>
-          <CText text="or" />
+          <Text text="or" />
         </View>
-        <Button text="Free Tracking" block />
+        <Button text="Sample Text" block />
         <View style={[GLOBAL.LAYOUT.justifyCenter, GLOBAL.LAYOUT.alignCenter, styles.bottomTextContainer]}>
-          <CText gray text="New?" />
-          <CText bold green text="Read more here" />
+          <Text bold green text="Read more here" onPress={() => setModalVisibility(true)} />
         </View>
       </View>
+      <Modal
+        isVisible={showModal}
+        buttons={modalButtons}
+        text="This gps tracker was developed as a hobby project, to get more familiar with React Native and as a technical showcase."
+      />
     </View>
   );
 };
